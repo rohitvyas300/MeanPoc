2a93687e011c7644108c5c073408c386568f122f
rohitvyas300 <rohitvyas300@gmail.com>
2020-03-24 18:36:19 +0530

    api to get records by bate

--- mean-demo/controllers/EmployeeController.js
+++ mean-demo/controllers/EmployeeController.js
@@ -8,25 +8,6 @@ exports.employee_list = function(req, res) {
       });
 };
 
-// Display detail page for a specific book.
-exports.employee_detail = function(req, res) {
-    console.log("god is great....");
-    console.log(req.query.EmpName);
-    if(req.query.EmpName == 'Rohit Vyas'){
-      Employee.find({ 'EmpName': 'Rohit Vyas' }, 'EmpNum JlEmail MasterCustomer', function (err, products) {
-            if (err) return next(err);
-              res.json(products);
-        });
-  }
-  if(req.query.author == 'Sing'){
-    Employee.find({ 'author': 'Sing' }, 'title', function (err, products) {
-        if (err) return next(err);
-          res.json(products);
-    });
-}
-  //res.send("tests in pregres");
-};
-
 // create new employee.
 exports.add_employee = function(req, res,next) {
   Employee.create(req.body, function (err, products) {
@@ -44,3 +25,100 @@ exports.delete_employee = function(req, res,next) {
         res.json(products);
       });
 };
+
+
+// UPDATE an employee.
+exports.update_employee = function(req, res,next) {
+  Employee.findByIdAndUpdate(req.params._id, req.body, function (err, products){
+        if (err) return next(err);
+        res.json(products);
+      });
+};
+
+// Display list based on date range.
+exports.employeeList_month1 = function(req, res) {
+  Employee.find({"AgileActualDate":{$gte:new Date('2020-01-01'), $lt:new Date("2020-01-23")}},'EmpNum JlEmail MasterCustomer AgilePlanDate',function (err, products) {
+    var count = products.length;
+    console.log(count);    
+    if (err) return next(err);
+        res.json(count);
+      });
+};
+
+
+// Display certification count based on date range.
+exports.certificate_count_monthwise = function(req, res) {
+  var fromdate = req.query.fromdate;
+  var todate = req.query.todate;
+  var count = {};
+  Employee.find({"AgilePlanDate":{$gte:new Date(fromdate), $lt:new Date(todate)}},function (err, AgilePlanDate) {
+    if (err) return next(err);
+    count.AgilePlanDate = AgilePlanDate.length;
+        
+        Employee.find({"AgileActualDate":{$gte:new Date(fromdate), $lt:new Date(todate)}} ,function (err, AgileActualDate) {
+        if (err) return next(err);
+        count.AgileActualDate = AgileActualDate.length;
+      
+               Employee.find({"AgileCompletionDate":{$gte:new Date(fromdate), $lt:new Date(todate)}} ,function (err, AgileCompletionDate) {
+                if (err) return next(err);
+                count.AgileCompletionDate = AgileCompletionDate.length;
+
+                    Employee.find({"AutomationToolPlanDate":{$gte:new Date(fromdate), $lt:new Date(todate)}} ,function (err, AutomationToolPlanDate) {
+                      if (err) return next(err);
+                      count.AutomationToolPlanDate = AutomationToolPlanDate.length;
+
+                          Employee.find({"AutomationToolActualDate":{$gte:new Date(fromdate), $lt:new Date(todate)}} ,function (err, AutomationToolActualDate) {
+                          if (err) return next(err);
+                          count.AutomationToolActualDate = AutomationToolActualDate.length;
+
+                              Employee.find({"AutomationToolCompletionDate":{$gte:new Date(fromdate), $lt:new Date(todate)}} ,function (err, AutomationToolCompletionDate) {
+                                if (err) return next(err);
+                                count.AutomationToolCompletionDate = AutomationToolCompletionDate.length;
+
+                                    Employee.find({"PrgLangPlanDate":{$gte:new Date(fromdate), $lt:new Date(todate)}} ,function (err, PrgLangPlanDate) {
+                                    if (err) return next(err);
+                                    count.PrgLangPlanDate = PrgLangPlanDate.length;
+
+                                        Employee.find({"PrgLangActualDate":{$gte:new Date(fromdate), $lt:new Date(todate)}} ,function (err, PrgLangActualDate) {
+                                        if (err) return next(err);
+                                        count.PrgLangActualDate = PrgLangActualDate.length;
+
+                                            Employee.find({"PrgLangCompletionDate":{$gte:new Date(fromdate), $lt:new Date(todate)}} ,function (err, PrgLangCompletionDate) {
+                                            if (err) return next(err);
+                                            count.PrgLangCompletionDate = PrgLangCompletionDate.length;
+                                            res.json(count);
+                                          });
+                                      });
+                                  });  
+                              });
+                          });
+                    });
+              });
+        });
+  }); 
+};
+
+
+//db.bios.find( { birth: { $gt: new Date('1940-01-01'), $lt: new Date('1960-01-01') } } )
+//find({"OrderDateTime":{ $gte:ISODate("2019-02-10"), $lt:ISODate("2019-02-21") }
+//}).pretty();
+
+// Display detail page for a specific book.
+exports.employee_detail = function(req, res) {
+    console.log("god is great....");
+    console.log(req.query.EmpName);
+    if(req.query.EmpName == 'Rohit Vyas'){
+      Employee.find({ 'EmpName': 'Rohit Vyas' }, 'EmpNum JlEmail MasterCustomer AgilePlanDate', function (err, products) {
+            if (err) return next(err);
+              res.json(products);
+        });
+  }
+  if(req.query.author == 'Sing'){
+    Employee.find({ 'author': 'Sing' }, 'title', function (err, products) {
+        if (err) return next(err);
+          res.json(products);
+    });
+}
+  //res.send("tests in pregres");
+};
+
--- mean-demo/routes/catalog.js
+++ mean-demo/routes/catalog.js
@@ -7,14 +7,24 @@ var employee_controller = require('../controllers/EmployeeController');
 // GET request for one Book.
 router.get('/about', employee_controller.employee_detail);
 
-// GET request for list of all Book items.
+// GET request for list of employee details.
 router.get('/', employee_controller.employee_list);
 
-// Post request for creating items.
+// Post request for creating employee.
 router.post('/create', employee_controller.add_employee);
 
-// delete request for employee items.
-router.delete('/:_id',  employee_controller.delete_employee);
+// delete request for employee.
+router.delete('/delete/:_id',  employee_controller.delete_employee);
+
+// Update request for employee items.
+router.put('/update/:_id', employee_controller.update_employee);
+
+// GET request based on month.
+router.get('/month', employee_controller.certificate_count_monthwise);
+
+// GET request for one employee on id.
+router.get('/single/:id', employee_controller.singleemployee_detail);
+
 
 
 module.exports = router;
